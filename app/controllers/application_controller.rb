dc8085aec06aa73ece5bbc836d80cc12f6856192
Keith VenHuizen <keith.venhuizen@gmail.com>
2020-11-05 16:12:32 -1000

    Update Pages layouts

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,12 @@ class ApplicationController < ActionController::Base
     protect_from_forgery prepend: true
     before_action :authenticate_user!
     before_action :set_global_user
+    
 
     def set_global_user
-        @user = current_user
-        @character = Character.find_by(name: @user.username)
+        if current_user
+            @user = current_user
+            @character = Character.find_by(name: @user.username)
+        end
     end
 end
