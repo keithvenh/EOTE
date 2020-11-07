89016559bf53cbbe7693501e398fd629bd24833d
Keith VenHuizen <keith.venhuizen@gmail.com>
2020-11-07 11:59:39 -1000

    Debug CharacterEquipments

--- db/migrate/20201107130544_add_brand_to_character_equipments.rb
+++ db/migrate/20201107130544_add_brand_to_character_equipments.rb
@@ -1,4 +1,4 @@
-class AddBrandToCharacterEquipments < ActiveRecord::Migration[6.0]
+class AddBrandToCharacterEquipment < ActiveRecord::Migration[6.0]
   def change
     add_column :character_equipments, :brand, :string
   end
