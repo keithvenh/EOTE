758751582c78c416184c0de287296db9bf706f0c
Keith VenHuizen <keith.venhuizen@gmail.com>
2020-11-09 07:33:59 -1000

    update index image and animation

--- app/javascript/scripts/datapad.js
+++ app/javascript/scripts/datapad.js
@@ -1,11 +1,9 @@
 document.addEventListener("DOMContentLoaded",function(){
 
-    document.getElementById('connection-tracker').style.marginLeft = "10px";
-    document.getElementById('connection-tracker').style.marginRight = "10px";
+    document.getElementById('connection-tracker').style.marginLeft = "250px";
     const connection = setInterval(() => {
         let margin = Math.floor(Math.random()*20)
         document.getElementById('connection-tracker').style.marginLeft = `${margin}%`;
-        document.getElementById('connection-tracker').style.marginRight = `${margin}%`;
         
     }, 5000)
 
