9bd9710659ef929709429b9d083fb59922f8cf26
Keith VenHuizen <keith.venhuizen@gmail.com>
2020-11-11 08:37:56 -1000

    Refactor and Redesign Planets

--- app/controllers/planets_controller.rb
+++ app/controllers/planets_controller.rb
@@ -1,13 +1,6 @@
 class PlanetsController < ApplicationController
   def index
-    @deep_core = Planet.where(region: "Deep Core").order('name ASC')
-    @core = Planet.where(region: "Core").order('name ASC')
-    @colonies = Planet.where(region: "Colonies").order('name ASC')
-    @inner_rim = Planet.where(region: "Inner Rim").order('name ASC')
-    @expansion = Planet.where(region: "Expansion Region").order('name ASC')
-    @mid_rim = Planet.where(region: "Mid Rim").order('name ASC')
-    @outer_rim = Planet.where(region: "Outer Rim").order('name ASC')
-    @unknown = Planet.where(region: "Unkown Regions").order('name ASC')
+    @planets = Planet.all.order('name ASC')
   end
 
   def new
